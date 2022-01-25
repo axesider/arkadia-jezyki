d762c94c875ddad7391fe5cc757d31d80a72bf9a
Paweł Karczmarczuk <axesider@gmail.com>
2022-01-25 16:55:08 +0100

    better update

--- update-check.lua
+++ update-check.lua
@@ -8,12 +8,10 @@ function Jezyki.updateCheck:getFile()
     return getMudletHomeDir() .. "/plugins/" .. self.plugin_name .. "/commits"
 end
 
-function Jezyki.updateCheck:getURL()
-    return "https://api.github.com/repos/" .. self.repo .. "/" .. self.plugin_name .. "/commits"
-end
-
 function Jezyki.updateCheck:checkNewVersion()
-    downloadFile(self.getFile(), self.getURL())
+    local url = "https://api.github.com/repos/" .. self.repo .. "/" .. self.plugin_name .. "/commits"
+    local filename = self:getFile()
+    downloadFile(filename, url)
     registerAnonymousEventHandler("sysDownloadDone", function(_, file)
         self:handle(file)
     end, true)
@@ -21,36 +19,38 @@ function Jezyki.updateCheck:checkNewVersion()
 end
 
 function Jezyki.updateCheck:handle(fileName)
-    if fileName ~= self.file then
+    if fileName ~= self:getFile() then
         return
     end
 
-    local JezykiState = scripts.state_store:get(self.storeKey) or {}
-
-    local file, s, contents = io.open(self.getFile())
+    local file, s, contents = io.open(fileName)
     if file then
         contents = yajl.to_value(file:read("*a"))
         io.close(file)
-        os.remove(self.getFile())
+        os.remove(self:getFile())
         local sha = contents[1].sha
-        if JezykiState.sha ~= nil and sha ~= JezykiState.sha then
+        local State = scripts.state_store:get(self.storeKey) or {}
+        if State.sha ~= nil and sha ~= State.sha then
             echo("\n")
             cecho("<CadetBlue>(skrypty)<tomato>: Plugin "..self.plugin_name .." posiada nowa aktualizacje. Kliknij ")
             cechoLink("<green>tutaj", [[Jezyki.updateCheck:update()]], "Aktualizuj", true)
             cecho(" <tomato>aby pobrac")
             echo("\n")
         end
-        JezykiState.sha = sha
-        scripts.state_store:set(self.storeKey, JezykiState)
+        self.update_sha = sha
     end
 end
 
 function Jezyki.updateCheck:update()
     scripts.plugins_installer:install_from_url("https://codeload.github.com/" .. self.repo .. "/" .. self.plugin_name .. "/zip/master")
+    local State = scripts.state_store:get(self.storeKey) or {}
+    State.sha = self.update_sha
+    scripts.state_store:set(self.storeKey, State)
 end
 
 Jezyki.updateCheck.coroutine = coroutine.create(function()
     Jezyki.updateCheck:checkNewVersion()
 end)
-tempTimer(5, function() coroutine.resume(Jezyki.updateCheck.coroutine) end)
+
+tempTimer(4.15, function() coroutine.resume(Jezyki.updateCheck.coroutine) end)
 
